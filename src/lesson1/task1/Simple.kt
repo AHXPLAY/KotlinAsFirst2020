391acbf545a07249484ea074482863e4870df861
AHXPLAY <AHXPLAY@gmail.com>
2020-09-10 20:09:36 +0300

    lesson1.task1 : доделал оставшиеся(lengthInMeters, angleInRadian, trackLength, travelMinutes)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,7 +74,9 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int{
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double{
+    return (vershoks + arshins * 16 + 48 * sagenes) * 0.04445
+}
 
 /**
  * Тривиальная (1 балл)
@@ -82,7 +84,9 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    return deg * (PI/180) + min * PI / (180 * 60) + sec * PI / (180 * 3600)
+}
 
 /**
  * Тривиальная (1 балл)
@@ -90,7 +94,9 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+    return  sqrt((abs(x1 - x2)).pow(2) + (abs(y1 -y2)).pow(2))
+}
 
 /**
  * Простая (2 балла)
@@ -109,7 +115,10 @@ fun thirdDigit(number: Int): Int {
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    //если подразумевается что поезд вышел со станции после 00:00 и прибыл до 23:59
+    return hoursArrive * 60 + minutesArrive - (hoursDepart * 60 + minutesDepart)
+}
 
 /**
  * Простая (2 балла)
